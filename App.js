a564da0e7b27d822fabeffe525011bdb66998476
Ermeson Alves <118680982+ermesonalvesdev@users.noreply.github.com>
2022-11-21 09:39:48 -0300

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  // Criação do construtor para criar o State nas Props
+  constructor(props){
+    super(props);
+    this.state = {
+      numero: 0
+    };
+    // Criação do bind da Function
+    this.numberAlterMais = this.numberAlterMais.bind(this);
+    this.numberAlterMenos = this.numberAlterMenos.bind(this);
+  };
+
+  // Function do Button
+  numberAlterMais(){
+    this.setState({
+      numero: this.state.numero + 1
+    })
+  }
+  numberAlterMenos(){
+    if (this.state.numero == - 0) {
+      alert('Number menor que ZERO (0)')
+      this.setState({
+        numero: 0,
+      })
+    }
+    else{
+      this.setState({
+        numero: this.state.numero - 1,
+      })
+    }
+  }
+
+  render(){
+    return(
+      <View style={styles.container}>
+        <Text style={styles.text}> {this.state.numero} </Text>
+        <Button 
+        title='Count+'
+        onPress={this.numberAlterMais} />
+        <Button
+        title='Count-' 
+        onPress={this.numberAlterMenos} />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,5 +57,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: '500'
   },
 });
